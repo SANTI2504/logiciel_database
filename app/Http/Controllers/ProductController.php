26ed981e77cbe4b93572a6b56955684abbbf5391
SANTI2504 <62526796+SANTI2504@users.noreply.github.com>
2022-04-05 17:58:30 -0500

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,7 +10,7 @@ class ProductController extends Controller
 
     public function index(){
         $products= Product::all();
-        return view('app.inventory.product.index', compact('products'));
+        return view('app.inventory.index', compact('products'));
     }
 
     public function create(){
